df52b966e42690c9757df78ee89b7403f8c9b2aa
Rafael Angel Martinez Montero <ramontero@uninorte.edu.co>
2024-09-15 00:33:39 -0500

    responsive update

--- src/components/Diagnostico.jsx
+++ src/components/Diagnostico.jsx
@@ -3,14 +3,14 @@ function Diagnostico({dofa}) {
   return (
 <div className="flex flex-wrap mt-4">
   {dofa.map((item) => (
-      <section className="w-1/2 p-4 ">
+      <section className="w-1/1  md:w-1/2 p-4">
         <div  className="p-4 border-2 ">
-          <h2 className="text-center">
+          <h2 className="text-center m-4">
             {item.type}
           </h2>
           <ol className="list-decimal ml-4 space-y-2 pl-4">
             {item.items.map((item2) => (
-              <li key={item2.id}>
+              <li >
                 {item2.description}
                 <p className="my-6">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quisquam deserunt corporis, officiis modi impedit maxime fugiat, libero exercitationem sed totam neque. Neque sunt officia assumenda aut sed, similique quis.
